a4f72251a3096db14153c1617c8caef61be948db
Aaditya Singh <singhaditya2763@gmail.com>
2023-12-29 16:02:32 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// App.js
 import { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
@@ -6,39 +5,35 @@ import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import NavbarBox from "./components/Navbar/navbar";
 import Spinner from 'react-bootstrap/Spinner';
-
 import { Provider } from "react-redux";
 import store from "./redux/store/Store";
-
 import Dashboard from "./pages/dashboard";
 import axios from "axios";
 
 function App() {
-  const [data, setdata] = useState([]);
-  const [loginuser, setloginuser] = useState("");
-  const [loading, setloading] = useState(false);
-  const [hasError, sethasError] = useState(false);
-  const [errormsg, setErrorMessage] = useState("");
+  const [data, setData] = useState([]);
+  const [loginUser, setLoginUser] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [hasError, setHasError] = useState(false);
+  const [errorMsg, setErrorMessage] = useState("");
+  const [show, setShow] = useState(false);
 
   const userHandler = (user) => {
-    setloginuser(user);
+    setLoginUser(user);
   };
 
   const fetchData = async () => {
     try {
-      setloading(true);
-      const response = await axios.get(
-        "https://602e7c2c4410730017c50b9d.mockapi.io/users"
-      );
- 
-      setdata(response.data);
-     setloading(false)
-     sethasError(false)
+      setLoading(true);
+      const response = await axios.get("https://602e7c2c4410730017c50b9d.mockapi.io/users");
+      setData(response.data);
+      setLoading(false);
+      setHasError(false);
     } catch (error) {
-      console.log(error.message);
+      console.error(error.message);
       setErrorMessage(error.message);
-      sethasError(true)
-      setloading(false);
+      setHasError(true);
+      setLoading(false);
     }
   };
 
@@ -50,65 +45,74 @@ function App() {
     fetchData();
   };
 
-  const [show, setShow] = useState(false);
+  const deleteUser = (id) => {
+    const remainingUsers = data.filter((userData) => userData.id !== id);
+    setData(remainingUsers);
+  };
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
   return (
     <Provider store={store}>
       <BrowserRouter>
         <div style={{ overflow: "hidden" }}>
-          {!loading && !hasError && (<NavbarBox loginuserHandler={userHandler} user={loginuser} />
-)}
-{loading && ( <div className="d-flex flex-direction-row   "  style={{margin:"300px auto", width:"200px" }}>
-  <Spinner animation="border" role="status" >
-      <span className=" text-dark"></span>
-    </Spinner> 
-    <span style={{
+          {!loading && !hasError && (
+            <NavbarBox loginuserHandler={userHandler} user={loginUser} />
+          )}
+          {loading && (
+            <div className="d-flex flex-direction-row" style={{ margin: "300px auto", width: "200px" }}>
+              <Spinner animation="border" role="status">
+                <span className="text-dark"></span>
+              </Spinner>
+              <span style={{
                 color: "black",
                 fontFamily: "IBM Plex Sans",
                 fontSize: "20px",
                 fontStyle: "normal",
-                fontWeight: 700, paddingLeft:10
+                fontWeight: 700,
+                paddingLeft: 10
               }}>Loading....</span>
-    </div>)}
-    { !loading &&  hasError &&(  <div
-      className="modal show"
-      style={{ display: 'block', position: 'initial',marginTop:"200px"}}
-    >
-      <Modal.Dialog>
-        <Modal.Header >
-          <Modal.Title className="text-danger">Unable to Fetch data</Modal.Title>
-        </Modal.Header>
-
-        <Modal.Body>
-          <p>{errormsg}</p>
-        </Modal.Body>
-
-        <Modal.Footer>
-        
-          <Button variant="success"  style={{ color: "black",
-                fontFamily: "IBM Plex Sans",
-                fontSize: "20px",
-                color:"white",
-                fontStyle: "normal",
-                fontWeight: 500,}} onClick={errorFetchDataHandler}> Try Again</Button>
-        </Modal.Footer>
-      </Modal.Dialog>
-    </div>)}
-          {!loading && !hasError &&  (<Routes>
-            <Route
-              path="/"
-              element={
-                <Dashboard
-                  data={data}
-                  errorFetchDataHandler={errorFetchDataHandler}
-                  loading={loading}
-                  errormsg={errormsg}
-                />
-              }
-            />
-          </Routes>)}
+            </div>
+          )}
+          {!loading && hasError && (
+            <div className="modal show" style={{ display: 'block', position: 'initial', marginTop: "200px" }}>
+              <Modal.Dialog>
+                <Modal.Header>
+                  <Modal.Title className="text-danger">Unable to Fetch data</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                  <p>{errorMsg}</p>
+                </Modal.Body>
+                <Modal.Footer>
+                  <Button variant="success" style={{
+                    color: "black",
+                    fontFamily: "IBM Plex Sans",
+                    fontSize: "20px",
+                    color: "white",
+                    fontStyle: "normal",
+                    fontWeight: 500,
+                  }} onClick={errorFetchDataHandler}> Try Again</Button>
+                </Modal.Footer>
+              </Modal.Dialog>
+            </div>
+          )}
+          {!loading && !hasError && (
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <Dashboard
+                    data={data}
+                    errorFetchDataHandler={errorFetchDataHandler}
+                    deleteUser={deleteUser}
+                    loading={loading}
+                    errorMsg={errorMsg}
+                  />
+                }
+              />
+            </Routes>
+          )}
         </div>
       </BrowserRouter>
     </Provider>
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -5,8 +5,8 @@ import images from "../../src/assets/dummy.svg"
 import { Container } from "react-bootstrap";
 import Spinner from 'react-bootstrap/Spinner';
 import { FaRegUser } from "react-icons/fa";
-const Dashboard = ({ data }) => {
-const[usersData,setUsersData]=useState(data)
+const Dashboard = ({ data ,deleteUser}) => {
+const[usersData,setUsersData]=useState([])
 const [loading,setloading]=useState(false)
 const [error,seterror]=useState(false)
 const [userId,setuserId]=useState("")
@@ -15,24 +15,27 @@ const[message,setmessage]=useState("")
 const [selectedUserId, setSelectedUserId] = useState(null);
 
 const userDeleteHandler=(id)=>{
-  console.log(id)
-
-const remainingUsers=data.filter((data)=>data.id!==id);
-console.log(remainingUsers)
+  
+deleteUser(id);
+setUsersData(data )
 setuser("")
 setmessage("User deleted successfully")
-setUsersData(remainingUsers);
 
 }
 
-useEffect(() => {
-  const timerId = setTimeout(() => {
-    setmessage("");
-  }, 1000);
 
+useEffect(() => {
+  setloading(true);
+  setUsersData(data)
+  const timerId = setTimeout(() => {
+   
+    setmessage("");
+    
+  }, 3000);
+  setloading(false);
   // Clear the timer when the component unmounts or when the data changes
   return () => clearTimeout(timerId);
-});
+},[data]);
 
   const userHandler=async(id)=>{
     try {
@@ -63,18 +66,18 @@ useEffect(() => {
   return (
     <div   style={{ overflow: "hidden" }} >
     
-      <marquee direction="left" className=" container-fluid  mt-1 text-danger text-center" style={{ fontFamily: "IBM Plex Sans",
+      <marquee direction="left" className=" container-fluid   text-danger text-center" style={{ fontFamily: "IBM Plex Sans",
                 fontSize: "22px",
                 fontStyle: "normal",
-                fontWeight: 600,}}>Note: <span className="text-success">Avatar of some users are not changing because the avatar fetched by the Api is incorrect. Please check ! </span></marquee>
-      <Container className="mt-3 ">
+                fontWeight: 600,}}>Note: <span className="text-success">Avatar of some users are not not visible because the avatar's link fetched by the Api is incorrect/not found. Please check ! Scroll down users list to see other users avatar. </span></marquee>
+      <Container className="mt-2 ">
         <div className="d-flex flex-direction-col  ">
           <Container
             className={classes.userContainer}
             style={{ width: "50%", height:"630px",borderBottom:"1px solid lightgray" }}
           >
             <h2
-              className="text-center p-2"
+              className="text-center p-2 position-sticky"
               
               style={{
                 color: "white",
@@ -83,30 +86,44 @@ useEffect(() => {
                 fontSize: "22px",
                 fontStyle: "normal",
                 fontWeight: 600,
-                position:"sticky",top:"0px",
+               top:0,
+               marginBottom:0,
               background:"#5A4FCF"
               }}
             >
-              User dashboard
+              Users List
             </h2>
-            {message && (            <div className="text-center border border-danger text-light  " style={{height:30,marginTop:-8,background:"#fd5c63"}}>User deleted successfully</div>
+            {message && (<div className="text-center border border-danger text-light  " style={{height:30,marginTop:-8,background:"#fd5c63"}}>User deleted successfully</div>
 )}
             {usersData.map((user,index) => {
               return (
-                <div key={index}  className="d-flex flex-direction-row justify-content-between border mt-0 " style={{height:"40", backgroundColor: selectedUserId === user.id ? '#CCCCFF' : 'white',}} onClick={()=>{
+                <div key={index}  className="d-flex flex-row justify-content-between border  align-items-center " style={{height:"80", backgroundColor: selectedUserId === user.id ? '#CCCCFF' : 'white',}} onClick={()=>{
                   userHandler(user.id)
                 }}>
-                  <div className="align-items-center">
-                    {" "}
-                    <img src={user.avatar} className="mt-1 mx-1 mb-1 rounded-circle border" style={{width:50,height:50}}/>
-                    <span className="m-2" style={{fontFamily: "IBM Plex Sans",
-                fontSize: "18px",
+                  <div className="d-flex flex-row align-items-center"  >
+                    
+                    <img src={user.avatar} className="mt-1 mx-1 mb-1 rounded-circle border" style={{width:"70px",height:"70px"}} />
+                 <div  >
+                 <div className="" style={{fontFamily: "IBM Plex Sans",
+                fontSize: "16px",
+              
                 fontStyle: "normal",
-                fontWeight: 400,}}>{user.profile.firstName}</span>
+                fontWeight: 400,}}>{user.profile.firstName}</div>
+                   <div className="" style={{fontFamily: "IBM Plex Sans",
+                fontSize: "14px",
+              color:"#6D747A",
+                fontStyle: "normal",
+                fontWeight: 400,}}>{user.jobTitle}</div>
+                   <div className="" style={{fontFamily: "IBM Plex Sans",
+                fontSize: "12px",
+                color:"#6D747A",  
+                fontStyle: "normal",
+                fontWeight: 400,}}>{user.profile.username}</div>
+                 </div>
                   </div>
-                  <Button variant="outline-primary" className="m-2" onClick={()=>{
+                  <Button variant="outline-primary" className="mx-2" onClick={()=>{
                     userHandler(user.id)
-                  }}>view profile</Button>
+                  }} style={{height:40,}}>profile</Button>
                 </div>
               );
             })}
@@ -116,20 +133,24 @@ useEffect(() => {
             style={{ width: "100%", borderRadius:"10px" }}
           >
             <h2
-              className=" text-center   p-2 bg-opacity-10 "
+              className=" text-center position-sticky  p-2  bg-opacity-10 "
               style={{
                 color: "white",
                 width: "100%",
+                height:50,
                 fontFamily: "IBM Plex Sans",
                 fontSize: "22px",
                 fontStyle: "normal",
                 fontWeight: 600,
                 background:"#5A4FCF",
-                
+                top:0,
               }}
             >
               User dashboard
             </h2>
+            {message && (           <div className="text-center border border-danger text-light  " style={{height:30,marginTop:-8,background:"#fd5c63"}}>User deleted successfully</div>
+)}
+            
             {loading && ( <div className="d-flex flex-direction-row   "  style={{margin:"250px auto", width:"200px" }}>
   <Spinner animation="border" role="status" >
       <span className=" text-dark"></span>
@@ -155,8 +176,8 @@ useEffect(() => {
 )}
 
 {!user ? (<div className={classes.userBox}>
-  <FaRegUser style={{fontSize:'15rem',marginTop:'50px'}}/>
-  <h2 style={{textAlign:"center",margin:'50px auto'}}>Select user to see Profile</h2>
+  <FaRegUser style={{fontSize:'10rem',marginTop:'50px'}}/>
+  <h2 style={{textAlign:"center",margin:'40px auto'}}>Select user from userlist to see user's Profile</h2>
 </div>):(<div>
  
                 <div  className="d-flex flex-column  mt-1 " style={{height:"40"}}>
@@ -198,11 +219,11 @@ useEffect(() => {
                 fontStyle: "normal",}}><span style={{color:"gray"}}>Email :</span><a >{user.profile.email}</a></p>
                   <p style={{width:"80%",margin:'0px auto',fontFamily: "IBM Plex Sans",
                 fontSize: "20px",
-                fontStyle: "normal",}}><span style={{color:"gray"}}>Bio :</span> {user.Bio}</p>
+                fontStyle: "normal",height:"auto"}}><span style={{color:"gray"}}>Bio :</span> {user.Bio}</p>
                   <p style={{width:"80%",margin:'0px auto',fontFamily: "IBM Plex Sans",
-                fontSize: "20px",
-                fontStyle: "normal",}}><span style={{color:"gray"}}>Avatar Link :</span> <a   href={user.avatar}
-                target="_blank" style={{textDecoration:""}}>{user.avatar}</a></p>
+                fontSize: "20px",overflowWrap:"break",
+                fontStyle: "normal",height:"auto"}}><span style={{color:"gray"}}>Avatar Link :</span> <a   href={user.avatar}
+                target="_blank" style={{textDecoration:"none"}}>{user.avatar}</a></p>
 
                   </div>
                   <Button variant="outline-danger " className="mx-2 mt-1" 
